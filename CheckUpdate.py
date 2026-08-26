6b52bad67ce09ea5ecce3c84b45d20a2dbbd671b
MasaGratoR <masagrator@gmail.com>
2026-08-26 15:20:46 +0200

    Update CheckUpdate.py

--- CheckUpdate.py
+++ CheckUpdate.py
@@ -2,10 +2,10 @@ from github import Github
 from urllib.request import Request, urlopen
 import json
 import re
-import sys
 
 class DATA:
     database = {}
+    cnmt_bids = {}
 
 def DownloadDatabase():
     g = Github()
@@ -29,34 +29,54 @@ def DownloadDatabase():
         print("Last version_dump update (YYYY/MM/DD):")
         print(commits[0].commit.committer.date)
         print("\n---\n")
+
+    # 1. Download versions.txt
     site = "https://github.com/blawar/titledb/raw/master/versions.txt"
     request_site = Request(site, headers={"User-Agent": "Mozilla/5.0"})
     text = urlopen(request_site).read().decode("ascii").split("\n")
     for line in text:
-        if (line.find("id") != -1):
+        if line.find("id") != -1:
             continue
         array = line.rstrip("\n").rstrip("\r").split("|")
-        if (len(array) < 3):
-            continue
-        if (array[2] == ""):
+        if len(array) < 3 or array[2] == "":
             continue
         DATA.database[array[0]] = int(int(array[2]) / 65536)
+
+    # 2. Download version_dump.txt
     site = "https://raw.githubusercontent.com/masagrator/version_dump/refs/heads/main/version_dump.txt"
     request_site = Request(site, headers={"User-Agent": "Mozilla/5.0"})
     text = urlopen(request_site).read().decode("ascii").split("\n")
     for line in text:
-        if (line.find("id") != -1):
+        if line.find("id") != -1:
             continue
         array = line.rstrip("\n").rstrip("\r").split("|")
-        if (len(array) < 3):
-            continue
-        if (array[2] == ""):
+        if len(array) < 3 or array[2] == "":
             continue
         version_value = int(int(array[2]) / 65536)
-        if (array[0] not in DATA.database):
+        if array[0] not in DATA.database:
             DATA.database[array[0]] = version_value
         elif DATA.database[array[0]] < version_value:
             DATA.database[array[0]] = version_value
+
+    # 3. Download cnmts.json to map Title IDs to Build IDs
+    print("Downloading cnmts.json...")
+    site2 = "https://github.com/blawar/titledb/raw/refs/heads/master/cnmts.json"
+    request_site = Request(site2, headers={"User-Agent": "Mozilla/5.0"})
+    raw_dump = urlopen(request_site).read().decode("UTF-8")
+    cnmt_dump = json.loads(raw_dump)
+
+    for entry in cnmt_dump.values():
+        if not isinstance(entry, dict) or "buildId" not in entry or not entry["buildId"]:
+            continue
+        tid = entry.get("titleId", "").upper()
+        if not tid:
+            continue
+        bid = entry["buildId"][:16].upper()
+        ver = entry.get("version", 0)
+        ver = int(ver) // 65536 if ver and int(ver) >= 65536 else int(ver or 0)
+
+        if tid not in DATA.cnmt_bids or ver >= DATA.cnmt_bids[tid]["version"]:
+            DATA.cnmt_bids[tid] = {"version": ver, "bid": bid}
 
 print("Downloading database...")
 DownloadDatabase()
@@ -72,121 +92,47 @@ for line in file:
     titleid = line[pos:pos+16].upper()
     if titleid[15:16] != "0":
         continue
-    versionColumn = line.split("|")[3]
-    pos = versionColumn.find(", v") + 3
-    if (versionColumn.find("<br>") == -1):
-        version = int(re.sub(r"\D", "", versionColumn[pos:pos+2]))
-    else:
-        pos = versionColumn.rfind("<br>")
-        pos = versionColumn.find(", v", pos) + 3
-        version = int(re.sub(r"\D", "", versionColumn[pos:pos+2]))
-    try:
-        latestUpdate = DATA.database[titleid[:13] + "800"]
-    except:
-        try:
-            latestUpdate = DATA.database[titleid]
-        except:
-            print(f"Titleid not found: {titleid}")
-            print(f"Title:{gameTitle}")
-            print("---")
-            continue
-    if (version != latestUpdate):
-        print(titleid)
-        print(f"Title:{gameTitle}")
-        print(f"Newest update: v{latestUpdate}")
-        print(f"Latest patch: v{version}")
-        if (line.count("`0100") > 1):
-            print("Game has more than one titleid! Possible mismatch")
 
-        print("---")
-
-print("------------------------\n")
-print("Searching for duplicated BIDs...")
-
-site = "https://raw.githubusercontent.com/masagrator/FPSLocker-Warehouse/refs/heads/v4/README.md"
-site2 = "https://github.com/blawar/titledb/raw/refs/heads/master/ncas.json"
-
-print("Downloading ncas.json...")
-request_site = Request(site2, headers={"User-Agent": "Mozilla/5.0"})
-raw_dump = urlopen(request_site).read().decode("UTF-8")
-print("Downloading finished.")
-DUMP = json.loads(raw_dump)
-
-BIDS = {}
-
-for line in readme_dump:
-    if line.find("| `0100") == -1:
-        continue
-    gameTitle = line.split("|")[1]
-    pos = line.find("| `0100") + 3
-    titleid = line[pos:pos+16].upper()
-    if titleid[15:16] != "0":
-        continue
     versionColumn = line.split("|")[3]
     pos2 = versionColumn.find(" `") + 2
-    pos = versionColumn.find(", v") +3
-    BID = versionColumn[pos2:pos2+16].upper()
-    if (versionColumn.find("<br>") == -1):
+    pos = versionColumn.find(", v") + 3
+
+    if versionColumn.find("<br>") == -1:
+        readmeBID = versionColumn[pos2:pos2+16].upper()
         version = int(re.sub(r"\D", "", versionColumn[pos:pos+2]))
     else:
         pos = versionColumn.rfind("<br>")
         pos2 = versionColumn.find(" `", pos) + 2
         pos = versionColumn.find(", v", pos) + 3
-        BID = versionColumn[pos2:pos2+16].upper()
+        readmeBID = versionColumn[pos2:pos2+16].upper()
         version = int(re.sub(r"\D", "", versionColumn[pos:pos+2]))
-    if BID not in BIDS.keys():
-        BIDS[BID] = 1
-    else: BIDS[BID] += 1
 
-BIDS_NOT_ENOUGH = []
+    try:
+        latestUpdate = DATA.database[titleid[:13] + "800"]
+    except KeyError:
+        try:
+            latestUpdate = DATA.database[titleid]
+        except KeyError:
+            print(f"Titleid not found: {titleid}")
+            print(f"Title:{gameTitle}")
+            print("---")
+            continue
 
-keys = list(BIDS.keys())
-len_keys = len(keys)
-for i in range(len(keys)):
-    if (i % 100 == 0): print("Step 1/3: %d/%d" % (i, len_keys), end="\r")
-    count = raw_dump.count("\"%s" % keys[i])
-    if (count > BIDS[keys[i]]):
-        BIDS_NOT_ENOUGH.append(keys[i])
-
-keys2 = list(DUMP.keys())
-TIDS = {}
-len_keys = len(keys2)
-for i in range(len_keys):
-    if (i % 10000 == 0): print("Step 2/3: %d/%d" % (i, len_keys), end="\r")
-    if ("buildId" not in DUMP[keys2[i]].keys()):
-        continue
-    if (DUMP[keys2[i]]["buildId"] == None):
-        continue
-    if DUMP[keys2[i]]["buildId"][:16] not in TIDS:
-        TIDS[DUMP[keys2[i]]["buildId"][:16]] = [DUMP[keys2[i]]["titleId"][:13] + "000"]
+    # Retrieve BID from cnmts.json (checks Update TID ending with '800' first, then Base TID)
+    update_tid = titleid[:13] + "800"
+    if update_tid in DATA.cnmt_bids:
+        newestBID = DATA.cnmt_bids[update_tid]["bid"]
+    elif titleid in DATA.cnmt_bids:
+        newestBID = DATA.cnmt_bids[titleid]["bid"]
     else:
-        string = DUMP[keys2[i]]["titleId"][:13] + "000"
-        if (string not in TIDS[DUMP[keys2[i]]["buildId"][:16]]): TIDS[DUMP[keys2[i]]["buildId"][:16]].append(DUMP[keys2[i]]["titleId"][:13] + "000")
+        newestBID = "N/A"
 
-TIDS_found = {}
-len_bid_keys = len(BIDS_NOT_ENOUGH)
-for i in range(len_bid_keys):
-    if (i % 100 == 0): print("Step 3/3: %d/%d" % (i, len_bid_keys), end="\r")
-    if (len(TIDS[BIDS_NOT_ENOUGH[i]]) == 1):
-        continue
-    count = BIDS[keys[i]]
-    for x in range(len(TIDS[BIDS_NOT_ENOUGH[i]])):
-        if (readme_dump.count(BIDS_NOT_ENOUGH[i]) != count):
-            TIDS_found[BIDS_NOT_ENOUGH[i]] = TIDS[BIDS_NOT_ENOUGH[i]]
+    if version != latestUpdate:
+        print(titleid)
+        print(f"Title:{gameTitle}")
+        print(f"Newest update: v{latestUpdate} ({newestBID})")
+        print(f"Latest patch: v{version} ({readmeBID})")
+        if line.count("`0100") > 1:
+            print("Game has more than one titleid! Possible mismatch")
 
-print("                                                 ")
-
-BLACKLIST = ["E4F041624093998D", "937209E79E2E0E5D", "473D222EB1BDAD47", "217C9ECF258C0312", "DFC7E8979528DE44", "2284DFB25F387719",
-            "0F73F1D52820F90B", "6D9EA94F8AAC00A8", "3749BFEA64DC98DF", "AA9BF85240409E60", "4BC4A8A814FD46A4", "B151A224A429F9A7",
-             "D27FD8A515077F34", "33DBE39C8A83F1E6", "A8C6A84FBCF08724", "372AB37327DB2C31", "11D7F970861DFA66", "39467922663ACFBC"]
-
-tid_keys = list(TIDS_found.keys())
-for i in range(len(tid_keys)):
-    if (tid_keys[i] in BLACKLIST):
-        continue
-    print("BID:", tid_keys[i])
-    for x in range(len(TIDS_found[tid_keys[i]])):
-        print("TID %d: %s" % (x, TIDS_found[tid_keys[i]][x]))
-    print("\n")
-
-print("Script finished execution.")
+        print("---")
